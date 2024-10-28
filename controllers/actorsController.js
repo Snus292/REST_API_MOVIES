97be4e33783af6e5d00cfc8891964ae5205080e5
Snus292 <georgiigiblov@gmail.com>
2024-10-28 13:41:40 +0200

    минималестичные исправления и добавление коментариев

--- controllers/actorsController.js
+++ controllers/actorsController.js
@@ -1,26 +1,26 @@
 const actorService = require('../services/actorService');
 
-// Контроллер для создания актера с валидацией
+
 exports.createActor = async (req, res) => {
     try {
         let { name } = req.body;
 
-        // Валидация: проверка наличия имени
+        // валидация: проверка наличия имени
         if (!name || typeof name !== 'string' || name.trim() === '') {
-            return res.status(400).json({ message: 'Поле "name" обязательно для заполнения и не может быть пустым' });
+            return res.status(400).json({ message: 'поле "name" обязательно для заполнения и не может быть пустым' });
         }
-        // Форматирование имени: удаление лишних пробелов и проверка на наличие цифр
-        name = name.trim().replace(/\s+/g, ' '); // Убираем лишние пробелы
-        if (!/^[a-zA-Zа\s]+$/.test(name)) { // Проверка на наличие только букв и пробелов
-            return res.status(400).json({ message: 'Имя может содержать только буквы и пробелы' });
+        // удаление лишних пробелов и проверка на наличие цифр
+        name = name.trim().replace(/\s+/g, ' '); // лишние пробелы
+        if (!/^[a-zA-Zа\s]+$/.test(name)) { // наличие только букв и пробелов
+            return res.status(400).json({ message: 'имя может содержать только буквы и пробелы' });
         }
 
-        // Проверка на существование актера с таким именем
+        // проверка на существование актера с таким именем
         const existingActor = await actorService.findActorByName(name); // Предполагается, что этот метод реализован в actorService
         if (existingActor) {
             return res.status(400).json({ message: 'Актер с таким именем уже существует' });
         }
-        // Создаем актера через сервис
+        // сосдать актера через сервис
         const actor = await actorService.createActor({ name });
         res.status(201).json(actor);
     } catch (error) {
@@ -28,7 +28,7 @@ exports.createActor = async (req, res) => {
         res.status(500).json({ message: 'Ошибка при создании актера', error });
     }
 };
-// Получение фильмов с фильтрацией и пагинацией
+//  фильмы с фильтрацией и пагинацией
 exports.getActors = async (req, res) => {
     try {
         const actors = await actorService.getActors(req.query);
@@ -43,7 +43,7 @@ exports.getActorById = async (req, res) => {
     try {
         const actor = await actorService.getActorById(req.params.id);
         if (!actor) {
-            return res.status(404).json({ message: 'Фильм не найден' });
+            return res.status(404).json({ message: 'фильм не найден' });
         }
         res.json(actor);
     } catch (error) {
